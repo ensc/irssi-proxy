c15b5891470a0e9e77b6e5624f7ae6a07aa59628
Enrico Scholz <enrico.scholz@informatik.tu-chemnitz.de>
2008-02-26 17:29:10 +0100

    PROXY: implemented native proxy support
    This patch creates a hook into the net_connect*() methods which call a
    method to connect to a proxy.
    
    Previous solution to send certain strings in the normal IRC dialog was
    some kind of hack as most proxies require some kind of negotation.
    
    E.g. HTTP proxies send a 'HTTP/1.0 200 Connection established' HTTP header
    and clients have to wait for it.  Else, sent bytes of the following IRC
    login will be dropped silently.
    
    With old method, it is also impossible to tunnel SSL IRC connections
    through the proxy as proxy speaks plain text or a special protocol while
    e.g. 'CONNECT ... HTTP/1.0' will be encrypted with key of IRC server.
    
    There are further enhancements possible: the whole net_connect stuff
    should be made asynchronously. Currently, only the hostname is resolved
    in the background (which makes little sense of local proxies usually).

--- src/core/network-openssl.c
+++ src/core/network-openssl.c
@@ -479,11 +479,11 @@ static GIOChannel *irssi_ssl_get_iochannel(GIOChannel *handle, const char *hostn
 	return gchan;
 }
 
-GIOChannel *net_connect_ip_ssl(IPADDR *ip, int port, const char* hostname, IPADDR *my_ip, const char *cert, const char *pkey, const char *cafile, const char *capath, gboolean verify)
+GIOChannel *net_connect_proxy_ssl(struct network_proxy const *proxy, const char* hostname, int port, IPADDR *ip, IPADDR *my_ip, const char *cert, const char *pkey, const char *cafile, const char *capath, gboolean verify)
 {
 	GIOChannel *handle, *ssl_handle;
 
-	handle = net_connect_ip(ip, port, my_ip);
+	handle = net_connect_proxy(proxy, hostname, port, ip, my_ip);
 	if (handle == NULL)
 		return NULL;
 	ssl_handle  = irssi_ssl_get_iochannel(handle, hostname, cert, pkey, cafile, capath, verify);
